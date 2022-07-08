4caef4dca81391f32844b7f01f966ca1d5d8773e
OgaAndAya <96669935+OgaAndAya@users.noreply.github.com>
2022-07-08 11:20:51 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+
+difference=0;
+leftWristX=0;
+rightWristX=0;
+
+function setup(){
+    video= createCapture(VIDEO);
+    video.size(550,500);
+    canvas= createCanvas(550,550);
+    canvas.position(560,150);
+    poseNet= ml5.poseNet(video,modelLoaded)
+    poseNet.on("pose", gotPoses);
+}
+
+function modelLoaded(){
+    console.log("Pose Net is Initialized.");
+}
+ function gotPoses(results){
+ if(results.length>0){
+     console.log(results);
+     leftWristX= results[0].pose.leftWrist.x;
+     rightWristX= results[0].pose.rightWrist.x;
+     difference= floor(leftWristX - rightWristX);
+     console.log("leftWristX= "+ leftWristX + "rightWristX="+ rightWristX + "difference=" + difference);
+
+ }
+ }
+  function draw(){
+      background("red");
+      document.getElementById("font_size").innerHTML= "Font Size of the text will be="+ difference+ "px";
+      textSize(difference);
+      fill("white");
+      text('Oga', 50,400);
+  }
